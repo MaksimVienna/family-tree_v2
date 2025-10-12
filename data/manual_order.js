331bcd05c3d301a602dbb73e9c72dfd7b667461c
Mona Lisa <savchenkomaximl@gmail.com>
2025-10-12 19:44:17 +0200

    personal webpages

--- data/manual_order.js
+++
@@ -1,8 +0,0 @@
-const manualOrderFull = {
-  "1": ["51"], // Generation 1: left-to-right individual person order (OPTIMIZED)
-  "2": ["32", "31", "52"], // Generation 2: left-to-right individual person order (OPTIMIZED)
-  "3": ["28", "27", "30", "29", "33", "36", "34", "38", "35", "44"], // Generation 3: left-to-right individual person order (OPTIMIZED)
-  "4": ["11", "10", "2", "1", "21", "22", "14", "15", "18", "19", "53", "37", "39", "41", "40", "43", "46", "45", "48"], // Generation 4: left-to-right individual person order (OPTIMIZED)
-  "5": ["23", "3", "4", "5", "6", "8", "9", "17", "16", "24", "20", "54", "55", "42", "49", "50", "47"], // Generation 5: left-to-right individual person order (OPTIMIZED)
-  "6": ["7", "12", "13", "25", "26"] // Generation 6: left-to-right individual person order (OPTIMIZED)
-};
