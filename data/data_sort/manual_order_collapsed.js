37f2d280fa0220aa1f431e094daac4eb614cd763
Mona Lisa <savchenkomaximl@gmail.com>
2025-10-12 19:47:13 +0200

    personal webpages

---
+++ data/data_sort/manual_order_collapsed.js
@@ -0,0 +1,8 @@
+const manualOrderCollapsed = {
+  "1": ["1074"], // Generation 1: left-to-right group order
+  "2": ["1073"], // Generation 2: left-to-right group order
+  "3": ["1071", "1072"], // Generation 3: left-to-right group order
+  "4": ["1067", "1066", "1068", "1069", "1070"], // Generation 4: left-to-right group order
+  "5": ["1058", "1059", "1060", "1061", "1065", "1062", "1063", "1064"], // Generation 5: left-to-right group order
+  "6": ["1055", "1056", "1057"] // Generation 6: left-to-right group order
+};
---
+++ data/data_sort/manual_order_old.js
@@ -0,0 +1,8 @@
+const manualOrder = {
+  "1": ["51"], // Generation 1: left-to-right node order
+  "2": ["52", "32", "31"], // Generation 2: left-to-right node order
+  "3": ["27", "28", "29", "30", "33", "36", "34", "38", "35", "44"], // Generation 3: left-to-right node order
+  "4": ["1", "2", "21", "22", "14", "15", "18", "19", "37", "53", "39", "40", "43", "41", "45", "48", "46", "10", "11"], // Generation 4: left-to-right node order
+  "5": ["23", "3", "4", "5", "6", "8", "9", "16", "24", "17", "20", "42", "47", "49", "50", "55", "54"], // Generation 5: left-to-right node order
+  "6": ["7", "12", "13", "25", "26"] // Generation 6: left-to-right node order
+};
