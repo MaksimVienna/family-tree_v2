c9077c649bc03936092ac28e6eca42d1576bbfcc
Mona Lisa <savchenkomaximl@gmail.com>
2025-11-29 12:13:27 +0100

    new people and details

--- docs/data/final_x_coordinates.js
+++ docs/data/final_x_coordinates.js
@@ -1,8 +1,8 @@
 const finalXCoordinates = {
-  "1": [ 10.0649, 11.0649, 12.1649, 13.1649 ], // Generation 1: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "2": [ 10.5649, 11.5649, 12.7649 ], // Generation 2: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "3": [ 4.9205, 5.9205, 8.9516, 9.9516, 11.1516, 12.1516, 13.5516, 14.5516, 15.9516, 16.9516 ], // Generation 3: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "4": [ 1.7205, 2.7205, 3.8205, 4.8205, 6.0205, 7.0205, 8.2205, 9.2205, 10.6205, 11.6205, 13.5205, 14.7205, 15.7205, 16.8205, 17.8205, 20.5205, 21.7205, 22.7205 ], // Generation 4: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "5": [ 0.0000, 1.1000, 2.1000, 3.3000, 4.3000, 5.5000, 6.7000, 8.6000, 9.8000, 10.8000, 11.9000, 14.6000, 16.0000, 17.0000, 18.1000, 19.3000, 20.4000, 21.6000, 22.7000 ], // Generation 5: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "6": [ 1.0000, 2.2000, 3.8000, 9.7000, 10.9000, 16.5000 ] // Generation 6: Final optimized x-coordinates (Array of X_coords, left-to-right)
+  "1": [ 7.0814, 8.0814, 14.6814, 15.6814 ], // Generation 1: Final optimized x-coordinates (Array of X_coords, left-to-right)
+  "2": [ 4.5814, 5.7814, 6.9814, 8.1814, 9.3814, 10.5814, 11.5814, 12.7814, 13.9814, 15.1814, 16.3814, 17.5814, 18.7814 ], // Generation 2: Final optimized x-coordinates (Array of X_coords, left-to-right)
+  "3": [ 4.9626, 5.9626, 8.9937, 9.9937, 11.1937, 12.1937, 13.5937, 14.5937, 15.9937, 16.9937 ], // Generation 3: Final optimized x-coordinates (Array of X_coords, left-to-right)
+  "4": [ 1.7626, 2.7626, 3.8626, 4.8626, 6.0626, 7.0626, 8.2626, 9.2626, 10.6626, 11.6626, 13.5626, 14.7626, 15.7626, 16.8626, 17.8626, 20.5626, 21.7626, 22.7626 ], // Generation 4: Final optimized x-coordinates (Array of X_coords, left-to-right)
+  "5": [ 0.0000, 1.1000, 2.3000, 3.5000, 4.5000, 5.7000, 6.7000, 8.6000, 9.8000, 10.8000, 11.9000, 14.6000, 16.0000, 17.0000, 18.1000, 19.3000, 20.4000, 21.6000, 22.7000 ], // Generation 5: Final optimized x-coordinates (Array of X_coords, left-to-right)
+  "6": [ 4.0000, 5.6000, 6.8000, 9.7000, 10.9000, 16.5000 ] // Generation 6: Final optimized x-coordinates (Array of X_coords, left-to-right)
 };
