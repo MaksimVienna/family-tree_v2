529d1c8b122613675d29d64afb9333065ba42b17
Mona Lisa <savchenkomaximl@gmail.com>
2025-12-02 21:55:10 +0100

    clean folders

--- docs/data/final_x_coordinates.js
+++
@@ -1,8 +0,0 @@
-const finalXCoordinates = {
-  "1": [ 7.0393, 8.0393, 14.6393, 15.6393 ], // Generation 1: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "2": [ 4.5393, 5.7393, 6.9393, 8.1393, 9.3393, 10.5393, 11.5393, 12.7393, 13.9393, 15.1393, 16.3393, 17.5393, 18.7393 ], // Generation 2: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "3": [ 4.9205, 5.9205, 8.9516, 9.9516, 11.1516, 12.1516, 13.5516, 14.5516, 15.9516, 16.9516 ], // Generation 3: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "4": [ 1.7205, 2.7205, 3.8205, 4.8205, 6.0205, 7.0205, 8.2205, 9.2205, 10.6205, 11.6205, 13.5205, 14.7205, 15.7205, 16.8205, 17.8205, 20.5205, 21.7205, 22.7205 ], // Generation 4: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "5": [ 0.0000, 1.1000, 2.1000, 3.3000, 4.3000, 5.5000, 6.7000, 8.6000, 9.8000, 10.8000, 11.9000, 14.6000, 16.0000, 17.0000, 18.1000, 19.3000, 20.4000, 21.6000, 22.7000 ], // Generation 5: Final optimized x-coordinates (Array of X_coords, left-to-right)
-  "6": [ 1.0000, 2.2000, 3.8000, 9.7000, 10.9000, 16.5000 ] // Generation 6: Final optimized x-coordinates (Array of X_coords, left-to-right)
-};
--- docs/data/manual_order_regrouped.js
+++
@@ -1,8 +0,0 @@
-const manualOrderFull = {
-  "1": ["59", "60", "51", "58"], // Generation 1: left-to-right individual person order (REGROUPED)
-  "2": ["66", "67", "68", "69", "70", "31", "32", "52", "61", "62", "63", "64", "65"], // Generation 2: left-to-right individual person order (REGROUPED)
-  "3": ["28", "27", "30", "29", "34", "38", "33", "36", "35", "44"], // Generation 3: left-to-right individual person order (REGROUPED)
-  "4": ["11", "10", "2", "1", "21", "22", "14", "15", "18", "19", "56", "39", "40", "53", "37", "46", "45", "48"], // Generation 4: left-to-right individual person order (REGROUPED)
-  "5": ["23", "9", "8", "6", "5", "4", "3", "17", "16", "24", "20", "41", "43", "57", "54", "55", "49", "50", "47"], // Generation 5: left-to-right individual person order (REGROUPED)
-  "6": ["12", "13", "7", "25", "26", "42"] // Generation 6: left-to-right individual person order (REGROUPED)
-};
