008b588c8d70fe10bb9899049b506cb989e30d09
Mona Lisa <savchenkomaximl@gmail.com>
2025-12-02 22:22:15 +0100

    image update1

--- docs/data/manual_order_collapsed_regrouped.js
+++
@@ -1,8 +0,0 @@
-const manualOrderCollapsed = {
-  "1": ["1090", "1091"], // Generation 1: left-to-right group order (REGROUPED)
-  "2": ["1089"], // Generation 2: left-to-right group order (REGROUPED)
-  "3": ["1087", "1088"], // Generation 3: left-to-right group order (REGROUPED)
-  "4": ["1083", "1082", "1085", "1084", "1086"], // Generation 4: left-to-right group order (REGROUPED)
-  "5": ["1074", "1075", "1076", "1077", "1078", "1081", "1079", "1080"], // Generation 5: left-to-right group order (REGROUPED)
-  "6": ["1070", "1071", "1072", "1073"] // Generation 6: left-to-right group order (REGROUPED)
-};
